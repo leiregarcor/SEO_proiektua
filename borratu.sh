d528fb2bf1498ec4531087c11df938f39894bdf3
Ena <49266493+eniivak@users.noreply.github.com>
2021-04-30 23:06:39 +0200

    Delete borratu.sh

--- borratu.sh
+++
@@ -1,6 +0,0 @@
- sudo rm -R /var/www/foroak/
-sudo rm /etc/apache2/sites-available/phpbb3.conf 
- sudo rm -R phpBB-3.3.3.zip 
-sudo rm -R phpBB3/
-sudo rm /etc/apache2/sites-enabled/phpbb3.conf 
-sudo sed -i 's/Listen 8080/ /' /etc/apache2/ports.conf
